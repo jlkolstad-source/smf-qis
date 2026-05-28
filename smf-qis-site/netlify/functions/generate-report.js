d0075573d3af71532fe84845116a6a45cd3d48b5
jlkolstad-source <jlkolstad@gmail.com>
2026-05-28 17:28:46 -0600

    Update generate-report v6

--- smf-qis-site/netlify/functions/generate-report.js
+++ smf-qis-site/netlify/functions/generate-report.js
@@ -1,4 +1,4 @@
-// v5 - native fetch with timeout
+// v6 - native fetch with timeout
 exports.handler = async function(event, context) {
   const headers = {
     'Access-Control-Allow-Origin': '*',
